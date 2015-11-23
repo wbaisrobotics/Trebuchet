be2ee3e7a0de26f972f9d88cd2b8bcbf5cd2daed
ghost <ghost@users.noreply.github.com>
2015-11-23 16:39:20 +0200

    Add main method and unit test template

---
+++ src/main/java/org/usfirst/frc/team4338/robot/App.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team4338.robot;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ src/test/java/org/usfirst/frc/team4338/robot/AppTest.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team4338.robot;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+	
+	public AppTest(String testName) {
+		super(testName);
+	}
+	
+	public void testApp() {
+		// TODO
+	}
+}
