be5892a2d8cecc737e740e6b4633b1da20caf648
ghost <ghost@users.noreply.github.com>
2016-01-21 16:03:09 +0200

    Add robot class template

---
+++ src/main/java/org/usfirst/frc/team4338/robot/Robot.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team4338.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void autonomousInit() {
+		super.autonomousInit();
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		super.autonomousPeriodic();
+	}
+
+	@Override
+	public void disabledInit() {
+		super.disabledInit();
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		super.disabledPeriodic();
+	}
+
+	@Override
+	public void robotInit() {
+		super.robotInit();
+	}
+	
+	@Override
+	public void teleopInit() {
+		super.teleopInit();
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+		super.teleopPeriodic();
+	}
+	
+	@Override
+	public void testInit() {
+		super.testInit();
+	}
+	
+	@Override
+	public void testPeriodic() {
+		super.testPeriodic();
+	}
+	
+}
