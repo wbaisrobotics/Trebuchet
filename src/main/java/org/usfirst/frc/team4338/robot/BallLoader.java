4cd81f800dc9419519d7e2d187dc3cd0be68767f
Crumster <crumster88@gmail.com>
2016-02-10 15:34:37 +0200

    Add BallLoader skeleton class

---
+++ src/main/java/org/usfirst/frc/team4338/robot/BallLoader.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team4338.robot;
+
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ *
+ */
+public class BallLoader {
+    private Victor loaderMotor;
+
+    public BallLoader(){
+
+    }
+
+    /**
+     *
+     */
+    public void loadBall(){
+
+    }
+
+    /**
+     *
+     */
+    public void loaderMoterOn(){
+
+    }
+}
