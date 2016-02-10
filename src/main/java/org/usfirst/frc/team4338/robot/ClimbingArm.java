c0d59a050a9010707a35effc435e4e36cf918242
Crumster <crumster88@gmail.com>
2016-02-10 15:37:39 +0200

    Add ClimbingArm skeleton

---
+++ src/main/java/org/usfirst/frc/team4338/robot/ClimbingArm.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team4338.robot;
+
+/**
+ *
+ */
+public class ClimbingArm {
+    public ClimbingArm(){
+
+    }
+
+    /**
+     *
+     */
+    public void deployArms(){
+
+    }
+
+    /**
+     *
+     */
+    public void climbTower(){
+
+    }
+}
