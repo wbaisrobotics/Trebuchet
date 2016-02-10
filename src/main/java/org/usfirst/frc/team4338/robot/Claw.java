3f078ad6ecd2e1b4f5699e47423755b72726996a
Crumster <crumster88@gmail.com>
2016-02-10 15:36:25 +0200

    Add Claw skeleton

---
+++ src/main/java/org/usfirst/frc/team4338/robot/Claw.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team4338.robot;
+
+/**
+ *
+ */
+public class Claw {
+    public Claw(){
+
+    }
+
+    /**
+     *
+     */
+    public void moveLeftClaw(){
+
+    }
+
+    /**
+     *
+     */
+    public void moveRightClaw(){
+
+    }
+
+    /**
+     * if position is determinable
+     */
+    public void setNeutral(){
+
+    }
+}
