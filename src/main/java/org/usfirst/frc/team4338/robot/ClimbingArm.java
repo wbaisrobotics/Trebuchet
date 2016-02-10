2a5e06b92ee23b98e1419641b429ec8ccff0d4f8
Crumster <crumster88@gmail.com>
2016-02-10 15:42:36 +0200

    Add Climbing arm motors and encoder

--- src/main/java/org/usfirst/frc/team4338/robot/ClimbingArm.java
+++ src/main/java/org/usfirst/frc/team4338/robot/ClimbingArm.java
@@ -1,9 +1,18 @@
 package org.usfirst.frc.team4338.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Victor;
+
 /**
  *
  */
 public class ClimbingArm {
+    private Victor leftArm;
+    private Victor rightArm;
+    private Victor winchMotor;
+    private Encoder climbDistance;
+    private boolean towerClimbed;
+
     public ClimbingArm(){
 
     }
