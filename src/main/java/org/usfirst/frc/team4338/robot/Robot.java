740dfce6693bbff25c74dbbc9e3d06d24fe23221
Crumster <crumster88@gmail.com>
2016-02-13 16:34:37 +0200

    Change driving x and y scale

--- src/main/java/org/usfirst/frc/team4338/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4338/robot/Robot.java
@@ -109,9 +109,9 @@ public class Robot extends IterativeRobot {
         angle = gyro.getAngle();
 
         double x = controller.getRightJoyX();
-        x = 0.75 * Math.signum(x) * Math.pow(x, 2);
+        x = 0.8 * Math.signum(x) * Math.pow(x, 2);
         double y = controller.getRightJoyY();
-        y = 0.75 * Math.signum(y) * Math.pow(y, 2);
+        y = 0.9 * Math.signum(y) * Math.pow(y, 2);
         drive.tankDrive(y - x, y + x);
 
         if(controller.getButtonRB()){
