8a8c4fd7fde44b5e3223a2bdbc1c0785e89c3f55
Crumster <crumster88@gmail.com>
2016-02-20 13:17:34 +0200

    Add comments to State enum

--- src/main/java/org/usfirst/frc/team4338/robot/State.java
+++ src/main/java/org/usfirst/frc/team4338/robot/State.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team4338.robot;
 
 /**
- *
+ * Enumeration of the different states that the robot can be in for various actions.
  */
 public enum State {
+    //ALL ID's MUST BE DIFFERENT
+    //ID's are in ascending order to indicate position of rotation.
     SQUAT(0, 0), LOAD(1, 0), TRAVEL(2, 0), LOWSHOT(3, 0), HIGHSHOT(4, 0);
 
     private int stateID;
@@ -14,10 +16,18 @@ public enum State {
         this.angle = angle;
     }
 
+    /**
+     * Gets the ID of the state
+     * @return
+     */
     public int getID(){
         return stateID;
     }
 
+    /**
+     * Gets the angle of shooter rotation for a given state
+     * @return
+     */
     public double getAngle(){
         return angle;
     }
